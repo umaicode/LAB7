f0f5978c3c932444e8503ad53debcd35011fabfa
umaicode <126807723+umaicode@users.noreply.github.com>
2023-10-31 16:44:14 +0900

    Update LAB7_1_201966241.ino
    Add lecture comment

--- LAB7_1_201966241/LAB7_1_201966241.ino
+++ LAB7_1_201966241/LAB7_1_201966241.ino
@@ -1,4 +1,5 @@
 #include <pitches.h>
+// 음정에 해당하는 주파수를 매크로로 설정.
 
 int speakerPin = 57;  // 스피커 연결 핀
 int melody[] = {  // 학교 종 멜로디, 0은 쉼표
@@ -15,6 +16,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // 멜로디 배열의 총 사이즈를 int형으로 나누어서 갯수 찾기.
   for(int thisNote = 0; thisNote < sizeof(melody) / sizeof(int); thisNote++) {
     // 음표 길이를 시간으로 변환
     int noteLength = 1000 / noteDuration;
@@ -23,5 +25,5 @@ void loop() {
     delay(noteLength);
     noTone(speakerPin); // 현재 음 재생 중지
   }
-  while(true);
+  while(true);  // 1회 재생 후 대기.
 }
